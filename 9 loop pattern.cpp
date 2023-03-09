b27e9e7202e703dc07b68927125c8d784919500d
jiyasavaliya18 <126229948+jiyasavaliya18@users.noreply.github.com>
2023-03-09 22:31:53 +0530

    Add files via upload

---
+++ 9 loop pattern.cpp
@@ -0,0 +1,21 @@
+//1 1 1 1 1
+//2 2 2 2
+//3 3 3
+//4 4
+//5
+
+#include<stdio.h>
+int main(){
+	
+	int r, c;
+	
+	for(r=1; r<=5; r++){
+		for(c=r; c<=5; c++){
+			printf("%d",r);
+			
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
